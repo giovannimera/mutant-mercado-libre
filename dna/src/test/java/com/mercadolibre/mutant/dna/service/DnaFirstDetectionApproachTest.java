d06bde07c7e3f3ea7b2db19a806a181a692192d5
Giovanni Mera <gmera@sura.com.co>
2021-04-09 20:01:04 -0500

    se corrije problema en la clase de pruebas

--- dna/src/test/java/com/mercadolibre/mutant/dna/service/DnaFirstDetectionApproachTest.java
+++ dna/src/test/java/com/mercadolibre/mutant/dna/service/DnaFirstDetectionApproachTest.java
@@ -20,7 +20,7 @@ import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DnaFirstDetectionApproachTest {
+class DnaFirstDetectionApproachTest {
 
 	@Autowired
 	private DnaDetection approach;
